12f2cff4f48cb4b9f8c0fe3d04030d2ddf3223b0
Kyle Estes <kyle.estes@polaris.com>
2018-02-14 19:30:27 -0600

    Harmless comment

--- App1/App1.Android/MainActivity.cs
+++ App1/App1.Android/MainActivity.cs
@@ -14,6 +14,7 @@ namespace App1.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+            // A harmless comment
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
